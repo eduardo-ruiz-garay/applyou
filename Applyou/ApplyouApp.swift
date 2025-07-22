d1daf2cbd53e039c300e4d0f7dce2ca3170b5fb5
eduardo-ruiz-garay <ruedudega@gmail.com>
2025-07-22 10:46:06 -0400

    Initial Commit

---
+++ Applyou/ApplyouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplyouApp.swift
+//  Applyou
+//
+//  Created by Eduardo Ruiz-Garay on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ApplyouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApplyouTests/ApplyouTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplyouTests.swift
+//  ApplyouTests
+//
+//  Created by Eduardo Ruiz-Garay on 7/22/25.
+//
+
+import Testing
+@testable import Applyou
+
+struct ApplyouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
